628cd39cdaff296e9e69422ccf2a119ddaa9a1a5
King Bean <doducvuong1304@gmail.com>
2024-08-13 22:18:28 +0700

    logger

--- jsonutilstool/src/main/java/com/kingbean/jsonutilstool/JsonUtilsApplication.java
+++ jsonutilstool/src/main/java/com/kingbean/jsonutilstool/JsonUtilsApplication.java
@@ -3,10 +3,12 @@ package com.kingbean.jsonutilstool;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.logging.Logger;
+
 @SpringBootApplication
 public class JsonUtilsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JsonUtilsApplication.class, args);
-		System.out.println("Json Utils Application");
+		Logger.getLogger(JsonUtilsApplication.class.getName()).info("Json Utils Application started");
 	}
 }
